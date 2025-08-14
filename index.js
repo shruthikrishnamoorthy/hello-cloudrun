bf850aa89ea0830c1d65f152ca7585c5ee7b11b5
shruthikrishnamoorthy <shruthi13.krishna@gmail.com>
2025-08-14 11:08:37 +0530

    updated ci-cd.yml

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
-const express = require('express');
+import express from 'express';
+
 const app = express();
-const PORT = process.env.PORT || 8080;
 
 app.get('/', (req, res) => {
   res.send('Hello World from Cloud Run 🚀');
 });
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+const port = process.env.PORT || 8080;
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
 });
